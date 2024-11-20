7ed43ffb1dc7b11ebcb6616a7e2f38e8cb8464ba
Loreb29 <102338909+Loreb29@users.noreply.github.com>
2024-11-19 23:51:00 -0500

    Update profesoresroutes.js
    Actu1

--- backend/routes/profesoresroutes.js
+++ backend/routes/profesoresroutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const profesorescontroller = require("../controllers/profesorescontrollers.js");
 
-router.get("/",profesorescontroller.consultar);
+router.route("/ecp/:id").get(profesorescontroller.consultarDetalle);
 router.post("/",profesorescontroller.ingresar);
 
 
 router.route("/:id")
-.get(profesorescontroller.consultarDetalle)
+.get(profesorescontroller.consultar)
 .put(profesorescontroller.actualizar)
 .delete(profesorescontroller.borrar);
 
