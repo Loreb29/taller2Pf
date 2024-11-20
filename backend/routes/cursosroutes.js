5d560bb2860e96b48cecc68d3485a2750d45cbfb
Loreb29 <102338909+Loreb29@users.noreply.github.com>
2024-11-19 23:51:11 -0500

    Update cursosroutes.js
    Actu2

--- backend/routes/cursosroutes.js
+++ backend/routes/cursosroutes.js
@@ -2,18 +2,21 @@ const express = require("express");
 const router = express.Router();
 const cursoscontroller = require("../controllers/CursosController.js");
 
-router.get("/",cursoscontroller.consultar);
+router.route("/ecp/:id").get(cursoscontroller.consultarDetalle);
+
 router.post("/",cursoscontroller.ingresar);
-router.get("/registraEstudiante",cursoscontroller.consultarEst);
+
+router.route("/registraEstudiante/ecp/:id").get(cursoscontroller.consultarDetalleEst);
+
 router.post("/registraEstudiante",cursoscontroller.asociarEstudiante);
 
 router.route("/:id")
-.get(cursoscontroller.consultarDetalle)
+.get(cursoscontroller.consultar)
 .put(cursoscontroller.actualizar)
 .delete(cursoscontroller.borrar);
 
 router.route("/registraEstudiante/:curso_id")
-.get(cursoscontroller.consultarDetalleEst)
+.get(cursoscontroller.consultarEst)
 .put(cursoscontroller.actualizarEst)
 .delete(cursoscontroller.borrarEst);
 module.exports = router;
